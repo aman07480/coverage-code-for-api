42ec0f52209d7f1e1568939feeffcb3622baa1b2
aman tamboli 07 <tamboliaman109@gmail.com>
2026-04-16 17:06:37 +0530

    written views file

---
+++ coverage/views.py
@@ -0,0 +1,34 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import APICoverage
+
+
+@api_view(["GET"])
+def api_coverage_list(request):
+
+    data = APICoverage.objects.all().values()
+
+    return Response({
+        "total_apis": len(data),
+        "data": data
+    })
+
+
+@api_view(["GET"])
+def unused_apis(request):
+
+    data = APICoverage.objects.filter(hit_count=0).values()
+
+    return Response({
+        "unused_apis": list(data)
+    })
+
+
+@api_view(["GET"])
+def top_used_apis(request):
+
+    data = APICoverage.objects.order_by("-hit_count")[:5].values()
+
+    return Response({
+        "top_apis": list(data)
+    })
