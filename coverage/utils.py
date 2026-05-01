60d00204c080fbd9e7adc2d5f3f7f24e2f7bf62d
aman tamboli 07 <tamboliaman109@gmail.com>
2026-05-01 22:06:14 +0530

    ipdated utils file

--- coverage/utils.py
+++ coverage/utils.py
@@ -9,3 +9,26 @@ def track_api_hit(path, method):
 
     obj.hit_count += 1
     obj.save()
+
+
+from .models import APICoverage
+# from .response_time_tracker import calculate_average_response_time
+
+def track_api_hit(path, method, response_time=0.0):
+
+    obj, created = APICoverage.objects.get_or_create(
+        endpoint=path,
+        method=method
+    )
+
+    obj.average_response_time = calculate_average_response_time(
+        obj.average_response_time,
+        obj.hit_count,
+        response_time
+    )
+
+    if response_time > obj.slowest_response_time:
+        obj.slowest_response_time = response_time
+
+    obj.hit_count += 1
+    obj.save()
